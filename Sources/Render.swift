8347f4df6dc1d22556161fc7006f3621c63a436b
Gabriel Handford <gabrielh@gmail.com>
2025-06-03 18:16:40 -0700

    Format

--- Sources/Render.swift
+++ Sources/Render.swift
@@ -4,20 +4,20 @@ import SwiftUI
 
 @MainActor
 public func assertRender(
-  view: some View,
-  fileID: StaticString = #fileID,
-  file: StaticString = #filePath,
-  testName: String = #function,
-  line: UInt = #line,
-  column: UInt = #column
+    view: some View,
+    fileID: StaticString = #fileID,
+    file: StaticString = #filePath,
+    testName: String = #function,
+    line: UInt = #line,
+    column: UInt = #column
 ) {
-  assertSnapshot(
-    of: view,
-    as: .imageRender,
-    fileID: fileID,
-    file: file,
-    testName: "\(testName).\(platformLabel)",
-    line: line,
-    column: column
-  )
+    assertSnapshot(
+        of: view,
+        as: .imageRender,
+        fileID: fileID,
+        file: file,
+        testName: "\(testName).\(platformLabel)",
+        line: line,
+        column: column
+    )
 }
--- Sources/Snapshot.swift
+++ Sources/Snapshot.swift
@@ -3,42 +3,42 @@ import SnapshotTesting
 import SwiftUI
 
 #if os(iOS)
-  @MainActor
-  public func assertSnapshot(
-    view: some View,
-    fileID: StaticString = #fileID,
-    file: StaticString = #filePath,
-    testName: String = #function,
-    line: UInt = #line,
-    column: UInt = #column
-  ) {
-    assertSnapshot(
-      of: view,
-      as: .image,
-      fileID: fileID,
-      file: file,
-      testName: "\(testName).\(platformLabel)",
-      line: line,
-      column: column
-    )
-  }
+    @MainActor
+    public func assertSnapshot(
+        view: some View,
+        fileID: StaticString = #fileID,
+        file: StaticString = #filePath,
+        testName: String = #function,
+        line: UInt = #line,
+        column: UInt = #column
+    ) {
+        assertSnapshot(
+            of: view,
+            as: .image,
+            fileID: fileID,
+            file: file,
+            testName: "\(testName).\(platformLabel)",
+            line: line,
+            column: column
+        )
+    }
 #else
-  @MainActor
-  public func assertSnapshot(
-    view: some View,
-    fileID: StaticString = #fileID,
-    file: StaticString = #filePath,
-    testName: String = #function,
-    line: UInt = #line,
-    column: UInt = #column
-  ) {
-    assertRender(
-      view: view,
-      fileID: fileID,
-      file: file,
-      testName: testName,
-      line: line,
-      column: column
-    )
-  }
+    @MainActor
+    public func assertSnapshot(
+        view: some View,
+        fileID: StaticString = #fileID,
+        file: StaticString = #filePath,
+        testName: String = #function,
+        line: UInt = #line,
+        column: UInt = #column
+    ) {
+        assertRender(
+            view: view,
+            fileID: fileID,
+            file: file,
+            testName: testName,
+            line: line,
+            column: column
+        )
+    }
 #endif
