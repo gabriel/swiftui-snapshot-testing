67bf612f8b9a3b2f21432bb2c231a171fd0eb58d
Gabriel Handford <gabrielh@gmail.com>
2025-06-07 22:18:26 -0700

    Lint

--- Sources/Expression.swift
+++ Sources/Expression.swift
@@ -8,34 +8,34 @@ import Testing
 ///   - timeout: The amount of time to wait to evaluate the expression.
 ///   - expression: An expression of Boolean type.
 public func expression(
-  timeout: TimeInterval = 1.0,
-  _ expression: @MainActor @escaping () async throws -> Bool
+    timeout: TimeInterval = 1.0,
+    _ expression: @MainActor @escaping () async throws -> Bool
 )
-  async throws -> Bool
+    async throws -> Bool
 {
-  actor ExpressionStatus {
-    var isFulfilled = false
-    func setIsFulfilled() {
-      isFulfilled = true
+    actor ExpressionStatus {
+        var isFulfilled = false
+        func setIsFulfilled() {
+            isFulfilled = true
+        }
     }
-  }
-  let start = CACurrentMediaTime()
-  let status = ExpressionStatus()
-  while await !status.isFulfilled, CACurrentMediaTime() - start < timeout {
-    await Task.yield()
-    let task = Task<Bool, Error> { @MainActor in
-      return try await expression()
+    let start = CACurrentMediaTime()
+    let status = ExpressionStatus()
+    while await !status.isFulfilled, CACurrentMediaTime() - start < timeout {
+        await Task.yield()
+        let task = Task<Bool, Error> { @MainActor in
+            return try await expression()
+        }
+        Task { [status] in
+            try await Task.sleep(for: .seconds(timeout))
+            if await !status.isFulfilled {
+                task.cancel()
+            }
+        }
+        // try await print("Evaluated expression: \(task.value)")
+        if try await task.value {
+            await status.setIsFulfilled()
+        }
     }
-    Task { [status] in
-      try await Task.sleep(for: .seconds(timeout))
-      if await !status.isFulfilled {
-        task.cancel()
-      }
-    }
-    // try await print("Evaluated expression: \(task.value)")
-    if try await task.value {
-      await status.setIsFulfilled()
-    }
-  }
-  return await status.isFulfilled
+    return await status.isFulfilled
 }
--- Tests/ExpressionTests.swift
+++ Tests/ExpressionTests.swift
@@ -4,40 +4,40 @@ import SwiftUISnapshotTesting
 import Testing
 
 struct MyView: View {
-  @State var model: MyViewModel
+    @State var model: MyViewModel
 
-  var body: some View {
-    if model.loaded {
-      Text("Loaded")
-    } else {
-      Text("Loading")
-        .task {
-          try? await model.load()
+    var body: some View {
+        if model.loaded {
+            Text("Loaded")
+        } else {
+            Text("Loading")
+                .task {
+                    try? await model.load()
+                }
         }
     }
-  }
 }
 
 @Observable @MainActor
 class MyViewModel {
-  var loaded = false
+    var loaded = false
 
-  func load() async throws {
-    try await Task.sleep(for: .milliseconds(100))
-    loaded = true
-  }
+    func load() async throws {
+        try await Task.sleep(for: .milliseconds(100))
+        loaded = true
+    }
 }
 
 @Test
 func testAsyncTask() async throws {
-  let model = await MyViewModel()
-  let view = MyView(model: model)
+    let model = await MyViewModel()
+    let view = MyView(model: model)
 
-  // Loading
-  await assertRender(view: view)
+    // Loading
+    await assertRender(view: view)
 
-  try #require(await expression { model.loaded })
+    try #require(await expression { model.loaded })
 
-  // Loaded
-  await assertRender(view: view)
+    // Loaded
+    await assertRender(view: view)
 }
